6b85b971e987528517d4db00ab5857a01a31f23e
tosbas <rootadm01@gmail.com>
2023-04-18 20:18:17 +0200

    Refacto générale, ajout d'inertie dans les mouvements

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -7,71 +7,13 @@ canvas.height = 600;
 // Web socket event
 const socket = io();
 
-// all clients
+// All clients
 let clients = [];
 
-/**
- * bottom
- */
-const event1 = () => {
-  let event1 = new KeyboardEvent("keydown", {
-    keyCode: 40,
-    repeat: true,
-  });
-  window.dispatchEvent(event1);
-};
-
-/**
- * Right
- */
-const event2 = () => {
-  let event2 = new KeyboardEvent("keydown", {
-    keyCode: 38,
-    repeat: true,
-  });
-  window.dispatchEvent(event2);
-};
-
-/**
- * Left
- */
-const event3 = () => {
-  let event3 = new KeyboardEvent("keydown", {
-    keyCode: 37,
-    repeat: true,
-  });
-  window.dispatchEvent(event3);
-};
-
-/**
- * Top
- */
-const event4 = () => {
-  let event4 = new KeyboardEvent("keydown", {
-    keyCode: 39,
-    repeat: true,
-  });
-  window.dispatchEvent(event4);
-};
-
-//The client
+// The client
 let client = undefined;
 
-//Position in x and y
-let posX = undefined;
-let posY = undefined;
-
-//Movement speed
-const speedMove = 0.5;
-
-//Radius
-const radius = 10;
-
-//Shadow in x and y, blur, size and color
-let shadowOffsetX = undefined;
-let shadowOffsetY = undefined;
-const shadowOffset = 10;
-const shadowBlur = 10;
+// Shadow color
 const shadowColor = "red";
 
 /**
@@ -81,46 +23,59 @@ const shadowColor = "red";
  */
 socket.on("connection", (e) => {
   document.querySelector("#name").innerHTML = socket.id;
-  //Empty the array containing all customers
+  // Empty the array containing all customers
   clients = [];
 
-  //Iterates through the array coming from the server
+  // Iterates through the array coming from the server
   e.forEach((element) => {
-    cellule(element.x, element.y, element.id);
+    cellule(
+      element.x,
+      element.y,
+      element.radius,
+      element.speed,
+      element.shadowOffset,
+      element.shadowBlur,
+      element.shadowOffsetX,
+      element.shadowOffsetY
+    );
     const client = {
       x: element.x,
       y: element.y,
       id: element.id,
+      radius: element.radius,
+      vx: element.vx,
+      vy: element.vy,
+      speed: element.speed,
+      acceleration: element.acceleration,
+      deceleration: element.deceleration,
+      shadowOffset: element.shadowOffset,
+      shadowBlur: element.shadowBlur,
       shadowOffsetX: element.shadowOffsetX,
       shadowOffsetY: element.shadowOffsetY,
     };
-    //Pushes each cell object into the "customers" array
+    // Pushes each cell object into the "customers" array
     clients.push(client);
   });
 
-  //Get client index, by socket.id
+  // Get client index, by socket.id
   const indexClient = searchIndexClient(socket.id);
 
-  //"Client" now contains the player
+  // "Client" now contains the player
   client = clients[indexClient];
-
-  /*posX and posY become equal to the value of the position x, y of the client
-   *and shadowOffsetX and shadowOffsetY to the value of the shadow in x and y of the client
-   */
-  posX = client.x;
-  posY = client.y;
-  shadowOffsetX = client.shadowOffsetX;
-  shadowOffsetY = client.shadowOffsetY;
 });
-
-//When a client moves, it sends a "move" event, which allows the real-time positions of the players to be updated on the client.
+ 
+// When a client moves, it sends a "move" event, which allows the real-time positions of the players to be updated on the client.
 socket.on("move", (e) => {
   //Recovery of the player who issued the event
   const indexClient = searchIndexClient(e.id);
 
-  //Recovery of the player who emitted the event by his index and updates the positions and the shadow in the "array" customers
+  // Recovery of the player who emitted the event by his index
   clients[indexClient].x = e.x;
   clients[indexClient].y = e.y;
+  clients[indexClient].radius = e.radius;
+  clients[indexClient].speed = e.speed;
+  clients[indexClient].shadowOffset = e.shadowOffset;
+  clients[indexClient].shadowBlur = e.shadowBlur;
   clients[indexClient].shadowOffsetX = e.shadowOffsetX;
   clients[indexClient].shadowOffsetY = e.shadowOffsetY;
 });
@@ -129,35 +84,24 @@ socket.on("disconnect", () => {
   console.log(socket.id);
 });
 
-/**
- * Function used to send the "client" object to the backend with in an event named "draw", 
- * updates the position in x and y, and its shadow in x and y
- * @param int posX
- * @param int posY
- * @param int shadowOffsetX
- * @param int shadowOffsetY
- * @returns void
- */
-const draw = (posX, posY, shadowOffsetX, shadowOffsetY) => {
-  client.x = posX;
-  client.y = posY;
+// Function used to send the "client" object to the backend with in an event named "draw"
+const draw = (x,y,radius,id,shadowOffset,shadowBlur,shadowOffsetX,shadowOffsetY) => {
+  client.x = x;
+  client.y = y;
+  client.radius = radius;
+  client.id = id;
+  client.shadowOffset = shadowOffset;
+  client.shadowBlur = shadowBlur;
   client.shadowOffsetX = shadowOffsetX;
   client.shadowOffsetY = shadowOffsetY;
   socket.emit("draw", client);
 };
 
-/**
- * Creating a cell
- * @param int x the position in x
- * @param int y the position in y
- * @param string his id
- * @param int shadowOffsetX The shadow in x
- * @param int shadowOffsetY The shadow in y
- * @returns void
- */
-const cellule = (x, y, id, shadowOffsetX, shadowOffsetY) => {
+// Creating a cell
+const cellule = (x,y,radius,shadowBlur,shadowOffset,shadowOffsetX,shadowOffsetY) => {
   ctx.beginPath();
   ctx.shadowColor = shadowColor;
+  ctx.shadowOffset = shadowOffset;
   ctx.shadowOffsetX = shadowOffsetX;
   ctx.shadowOffsetY = shadowOffsetY;
   ctx.shadowBlur = shadowBlur;
@@ -166,184 +110,130 @@ const cellule = (x, y, id, shadowOffsetX, shadowOffsetY) => {
   ctx.fill();
 };
 
-/**
- *Allows you to continuously clean the canvas, and browse the "arrayTouch" array to draw all the cells
- */
-const anim = () => {
-  //Clean the canvas
+function update() {
+  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //The function will call itself
-  requestAnimationFrame(anim);
-  //Iterates through the array "clients" and draws a cell for each cell stored in the array
+
   clients.forEach((element) => {
     cellule(
       element.x,
       element.y,
-      element.id,
+      element.radius,
+      element.shadowBlur,
+      element.shadowOffset,
       element.shadowOffsetX,
       element.shadowOffsetY
     );
   });
-};
 
-//Call of the function
-anim();
+  if (client !== undefined) {
+    // Calculate the new position of the cell based on its speed
+    client.x += client.vx;
+    client.y += client.vy;
 
-/**
- * Search client by id in array "client"
- * @param string id
- * @returns index of Client
- */
+    // Manage canvas overflow
+    if (client.x < 0 + client.radius) {
+      client.x = client.radius;
+      client.vx *= -0.5;
+    }
+    if (client.x > canvas.width - client.radius) {
+      client.x = canvas.width - client.radius;
+      client.vx *= -0.5;
+    }
+    if (client.y < 0 + client.radius) {
+      client.y = client.radius;
+      client.vy *= -0.5;
+    }
+    if (client.y > canvas.height - client.radius) {
+      client.y = canvas.height - client.radius;
+      client.vy *= -0.5;
+    }
+
+    // Manage the movement of the cell
+    if (isKeyPressed("ArrowUp")) {
+      // Speed up
+      client.vy -= client.acceleration;
+      client.shadowOffsetY = client.shadowOffset;
+    }
+    if (isKeyPressed("ArrowDown")) {
+      // Speed down
+      client.vy += client.acceleration;
+      client.shadowOffsetY =- client.shadowOffset;
+    }
+    if (isKeyPressed("ArrowLeft")) {
+      // Speed left
+      client.vx -= client.acceleration;
+      client.shadowOffsetX = client.shadowOffset;
+    }
+    if (isKeyPressed("ArrowRight")) {
+      // Speed right
+      client.vx += client.acceleration;
+      client.shadowOffsetX =- client.shadowOffset;
+    }
+
+    // Manage deceleration
+    if (!isKeyPressed("ArrowUp") && !isKeyPressed("ArrowDown")) {
+      if (client.vy > 0) {
+        client.vy = Math.max(0, client.vy - client.deceleration);
+      } else {
+        client.vy = Math.min(0, client.vy + client.deceleration);
+      }
+      client.shadowOffsetY = 0;
+    }
+
+    if (!isKeyPressed("ArrowLeft") && !isKeyPressed("ArrowRight")) {
+      if (client.vx > 0) {
+        client.vx = Math.max(0, client.vx - client.deceleration);
+      } else {
+        client.vx = Math.min(0, client.vx + client.deceleration);
+      }
+      client.shadowOffsetX = 0;
+    }
+
+    // Limit the speed of the cell
+    client.speed = Math.sqrt(client.vx * client.vx + client.vy * client.vy);
+    if (client.speed > 2) {
+      client.vx *= 2 / client.speed;
+      client.vy *= 2 / client.speed;
+    }
+
+    // Send new coordinates to backend
+    draw(
+      client.x,
+      client.y,
+      client.radius,
+      client.id,
+      client.shadowOffset,
+      client.shadowBlur,
+      client.shadowOffsetX,
+      client.shadowOffsetY
+    );
+  }
+
+  requestAnimationFrame(update);
+}
+
+let keys = {};
+
+document.addEventListener("keydown", function (event) {
+  keys[event.code] = true;
+});
+
+document.addEventListener("keyup", function (event) {
+  keys[event.code] = false;
+
+});
+
+// Function to check if a key is pressed
+function isKeyPressed(key) {
+  return keys[key] === true;
+}
+
+// Function to search for a client index by its id
 const searchIndexClient = (id) => {
   const indexClient = clients.findIndex((x) => x.id === id);
 
   return indexClient;
 };
 
-//Arrray which will contain the pressed keys
-let arrayTouch = [];
-
-//Event when a key is pressed
-window.addEventListener("keydown", (e) => {
-  //Reset shadow in x and y
-  shadowOffsetX = 0;
-  shadowOffsetY = 0;
-
-  //Get the code of the key pressed
-  const keycode = e.keyCode;
-
-  //Pushes the key code into the "arrayTouch" array
-  arrayTouch.push(keycode);
-
-  //Purge duplicates in the array "arrayTouch"
-  arrayTouch = arrayTouch.filter((x, i, a) => a.indexOf(x) == i);
-
-  //Detect cell position to avoid canvas overflow
-  if (posX + radius >= canvas.width) {
-    posX = canvas.width - radius;
-  }
-
-  if (posY + radius >= canvas.height) {
-    posY = canvas.height - radius;
-  }
-
-  if (posX - radius <= 0) {
-    posX = 0 + radius;
-  }
-
-  if (posY - radius <= 0) {
-    posY = 0 + radius;
-  }
-
-  /** 
-    Detects the keys contained in the "arrayTouch" array, 
-    adjusts the x and y position of the cell, and its shadow
-  */
-  if (
-    (arrayTouch[0] == 39 || arrayTouch[0] == 38) &&
-    (arrayTouch[1] == 38 || arrayTouch[1] == 39)
-  ) {
-    posX += speedMove;
-    posY -= speedMove;
-    shadowOffsetX = -shadowOffset;
-    shadowOffsetY = shadowOffset;
-  } else if (arrayTouch[0] == 39) {
-    posX += speedMove;
-    shadowOffsetX = -shadowOffset;
-  } else if (arrayTouch[0] == 38) {
-    posY -= speedMove;
-    shadowOffsetY = shadowOffset;
-  }
-
-  if (
-    (arrayTouch[0] == 39 || arrayTouch[0] == 40) &&
-    (arrayTouch[1] == 40 || arrayTouch[1] == 39)
-  ) {
-    posX += speedMove;
-    posY += speedMove;
-    console.log("ici");
-    shadowOffsetX = -shadowOffset;
-    shadowOffsetY = -shadowOffset;
-  } else if (arrayTouch[0] == 39) {
-    console.log("ici2");
-    posX += speedMove;
-    shadowOffsetX = -shadowOffset;
-  } else if (arrayTouch[0] == 40) {
-    posY += speedMove;
-    console.log("ici3");
-    shadowOffsetY = shadowOffset;
-  }
-
-  if (
-    (arrayTouch[0] == 37 || arrayTouch[0] == 40) &&
-    (arrayTouch[1] == 40 || arrayTouch[1] == 37)
-  ) {
-    posX -= speedMove;
-    posY += speedMove;
-    shadowOffsetX = shadowOffset;
-    shadowOffsetY = -shadowOffset;
-  } else if (arrayTouch[0] == 37) {
-    posX -= speedMove;
-    shadowOffsetX = shadowOffset;
-  } else if (arrayTouch[0] == 40) {
-    posY += speedMove;
-    shadowOffsetY = -shadowOffset;
-  }
-
-  if (
-    (arrayTouch[0] == 37 || arrayTouch[0] == 38) &&
-    (arrayTouch[1] == 38 || arrayTouch[1] == 37)
-  ) {
-    posX -= speedMove;
-    posY -= speedMove;
-    shadowOffsetX = shadowOffset;
-    shadowOffsetY = shadowOffset;
-  } else if (arrayTouch[0] == 37) {
-    posX -= speedMove;
-    shadowOffsetX = shadowOffset;
-  } else if (arrayTouch[0] == 38) {
-    posY -= speedMove;
-    shadowOffsetY = shadowOffset;
-  }
-
-  //Calls the draw function with the new x and y position, and the cell shadow in x and y
-  draw(posX, posY, shadowOffsetX, shadowOffsetY);
-});
-
-/**
- * Create a continuous listening on the pressed key, because during the diagonal movement,
- * if a key remains pressed, the keydown event stops, so to return to the keydown event,
- * I go through a custom event which allows me to return to the keydown
- * this is all in a setinterval to stay tuned
- */
-setInterval(() => {
-  switch (arrayTouch[0]) {
-    case 40:
-      event1();
-      break;
-    case 38:
-      event2();
-      break;
-    case 37:
-      event3();
-      break;
-    case 39:
-      event4();
-      break;
-  }
-}, 10);
-
-//Events when a key is released
-window.addEventListener("keyup", (e) => {
-  //Get the code of the key released
-  const keyCode = e.keyCode;
-  //Look for the index of the released key in the array "arrayTouch"
-  const indexTouch = arrayTouch.findIndex((x) => x === keyCode);
-  //Remove the key by its index return by "indexTouch"
-  arrayTouch.splice(indexTouch, 1);
-  //Reset shadow in x and y
-  client.shadowOffsetX = 0;
-  client.shadowOffsetY = 0;
-  //Sends the client object to the backend
-  socket.emit("draw", client);
-});
+update();
--- serveur/index.js
+++ serveur/index.js
@@ -13,23 +13,35 @@ app.get("/", function (req, res) {
 
 let clients = [];
 
+const radius = 10;
+const shadowOffset = 5;
+const shadowBlur = 10;
+let shadowOffsetX = 0;
+let shadowOffsetY = 0;
+
 class newPlayer {
   constructor(id) {
     this.id = id;
     this.x = Math.random() * 500;
     this.y = Math.random() * 500;
-    this.shadowOffsetX = 0;
-    this.shadowOffsetY = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 0;
+    this.acceleration = 0.01;
+    this.deceleration = 0.01;
+    this.shadowOffsetX = shadowOffsetX;
+    this.shadowOffsetY = shadowOffsetY;
+    this.shadowBlur = shadowBlur;
+    this.radius = radius;
+    this.shadowOffset = shadowOffset;
   }
 }
 
 io.on("connection", (socket) => {
-  console.log("nouvelle connection");
   clients.push(new newPlayer(socket.id));
   io.emit("connection", clients);
 
   socket.on("disconnect", (e) => {
-    console.log("Déconnection");
     const indexClient = searchIndexClient(socket.id);
     clients.splice(indexClient, 1);
     io.emit("connection", clients);
@@ -37,19 +49,22 @@ io.on("connection", (socket) => {
 
   socket.on("draw", (e) => {
     const indexClient = searchIndexClient(socket.id);
+
     clients[indexClient].x = e.x;
     clients[indexClient].y = e.y;
+    clients[indexClient].radius = radius;
+    clients[indexClient].id = socket.id;
+    clients[indexClient].vx = e.vx;
+    clients[indexClient].vy = e.vy;
+    clients[indexClient].shadowOffset = shadowOffset;
+    clients[indexClient].shadowBlur = shadowBlur;
     clients[indexClient].shadowOffsetX = e.shadowOffsetX;
     clients[indexClient].shadowOffsetY = e.shadowOffsetY;
     io.emit("move", clients[indexClient]);
   });
 });
 
-/**
- * Search client by socket id in array clients
- * @param string id
- * @returns index of Client
- */
+// Search client by socket id in array clients
 const searchIndexClient = (id) => {
   const indexClient = clients.findIndex((x) => x.id === id);
 
